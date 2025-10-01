a8352a079f859f2f587e0d54e09f70bf03e18ce6
Eduardo Girotto De Oliveira <25.00738-6@maua.br>
2025-10-01 09:27:08 -0300

    remocao de redundancias css

--- Codigo_Projeto/JS/script-menu.js
+++ Codigo_Projeto/JS/script-menu.js
@@ -1,11 +1,9 @@
 var ul = document.querySelector('header nav .parte-do-meio ul');
 
-// Função única para abrir/fechar
 function menuToggle() {
     ul.classList.toggle('open');
 }
 
-// Opcional: Fechar o menu ao clicar em um link (boa prática no mobile)
 var links = document.querySelectorAll('header nav .parte-do-meio ul li a');
 
 links.forEach(function(link) {
