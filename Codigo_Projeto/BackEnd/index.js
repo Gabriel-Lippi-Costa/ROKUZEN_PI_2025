87233af9191d53517c37e14113a8162038f1b128
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-10-22 16:48:59 -0300

    Começando o BackEnd

---
+++ Codigo_Projeto/BackEnd/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// app.post('/Codigo_Projeto/HTML/autenticacao.html', (req, res) => {
+
+// })
+
+app.listen(3000, () => {console.log('server up & running');
+})
