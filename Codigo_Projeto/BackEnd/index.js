d4000f75a033bbb700c200b08c6cdc46a8ca0738
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-10-22 17:06:32 -0300

    Arrumando o forms

--- Codigo_Projeto/BackEnd/index.js
+++ Codigo_Projeto/BackEnd/index.js
@@ -4,9 +4,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.post('/Codigo_Projeto/HTML/autenticacao.html', (req, res) => {
+app.post('/Codigo_Projeto/HTML/autenticacao.html', (req, res) => {
 
-// })
+})
 
 app.listen(3000, () => {console.log('server up & running');
 })
