7eeaf602d3ca27089330a600ff757ed1a1d02f0a
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-10-23 09:17:28 -0300

    Arrumando validação de telefone

--- Codigo_Projeto/JS/validacao-telefone.js
+++ Codigo_Projeto/JS/validacao-telefone.js
@@ -2,9 +2,10 @@ function validarTelefone(input) {
     let telefone = input.value.replace(/\D/g, "");
     let msg = document.querySelector("#msgTEL");
 
-    if (telefone.length <= 10) {
-        input.value = telefone.replace(/(\d{0,2})(\d{0,4})(\d{0,4})/,
-            function (_, p1, p2, p3) {
+    if (telefone.length > 0) {
+        if (telefone.length <= 10) {
+            // telefone fixo ou celular antigo sem 9
+            input.value = telefone.replace(/(\d{0,2})(\d{0,4})(\d{0,4})/, function(_, p1, p2, p3) {
                 let out = "";
                 if (p1) out += "(" + p1;
                 if (p1 && p1.length === 2) out += ") ";
@@ -12,9 +13,9 @@ function validarTelefone(input) {
                 if (p3) out += "-" + p3;
                 return out;
             });
-    } else {
-        input.value = telefone.replace(/(\d{0,2})(\d{0,5})(\d{0,4})/,
-            function (_, p1, p2, p3) {
+        } else if (telefone.length === 11) {
+            // celular novo com 9 dígitos
+            input.value = telefone.replace(/(\d{0,2})(\d{0,5})(\d{0,4})/, function(_, p1, p2, p3) {
                 let out = "";
                 if (p1) out += "(" + p1;
                 if (p1 && p1.length === 2) out += ") ";
@@ -22,14 +23,20 @@ function validarTelefone(input) {
                 if (p3) out += "-" + p3;
                 return out;
             });
-    }
+        } else {
+            msg.textContent = "❌ Número inválido";
+            msg.style.color = "red";
+            return;
+        }
 
-    if (telefone.length === 10 || telefone.length === 11) {
-        msg.textContent = "✅ Telefone válido";
-        msg.style.color = "green";
-    } else if (telefone.length > 0) {
-        msg.textContent = "❌ Número incompleto";
-        msg.style.color = "red";
+        // validação
+        if (telefone.length === 10 || telefone.length === 11) {
+            msg.textContent = "✅ Telefone válido";
+            msg.style.color = "green";
+        } else {
+            msg.textContent = "❌ Número incompleto";
+            msg.style.color = "red";
+        }
     } else {
         msg.textContent = "";
     }
